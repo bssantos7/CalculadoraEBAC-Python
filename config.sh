dfcac70a32a223c67318d71811ad423104732a36
bssantos7 <bs.santos7@hotmail.com>
2024-08-14 18:01:22 -0300

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Atualização e instalação do python 
+
+echo "inicio script instalação / atualização python"
+
+sudo apt update
+sudo apt install python3
+
+echo "fim script instação/atualização do python"
+
+#Atualização e instalação do gnuplot-nox para criar gráficos
+sudo apt install gnuplot-nox
+
+
+
